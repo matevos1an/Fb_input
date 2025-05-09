994175b2fe3fd5450e60061cb7edf9b4d09901c9
matevos1an <garr_2001@mail.ru>
2025-05-09 13:54:46 +0400

    first

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import './App.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { SchemaInput } from './schema/schema';
+
+function App() {
+  return (
+
+
+    <div>
+
+<h1 className="facebook-title">Facebook</h1>
+
+      <Formik
+        initialValues={{
+          login: "",
+          email: "",
+          password: "",
+          confirmPassword: ""
+        }}
+        onSubmit={(val) => console.log(val)}
+        validationSchema={SchemaInput}
+        validateOnBlur
+      >
+        <Form>
+          <Field name="login" placeholder="Login" />
+          <ErrorMessage name="login">
+            {(msg) => <p className="a">{msg}</p>}
+          </ErrorMessage>
+
+          <Field name="email" placeholder="Email" />
+          <ErrorMessage name="email">
+            {(msg) => <p className="a">{msg}</p>}
+          </ErrorMessage>
+
+          <Field name="password" placeholder="Password" type="password" />
+          <ErrorMessage name="password">
+            {(msg) => <p className="a">{msg}</p>}
+          </ErrorMessage>
+
+          <Field name="confirmPassword" placeholder="Confirm Password" type="password" />
+          <ErrorMessage name="confirmPassword">
+            {(msg) => <p className="a">{msg}</p>}
+          </ErrorMessage>
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/schema/schema.js
@@ -0,0 +1,35 @@
+import * as YUP from "yup";
+
+export const SchemaInput = YUP.object().shape({
+  login: YUP
+    .string()
+    .min(8, "Login must be at least 8 characters")
+    .max(16, "Login must be at most 16 characters")
+    .required("Login is required"),
+
+  email: YUP
+    .string()
+    .email("Invalid email format")
+    .required("Email is required"),
+
+  password: YUP
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(16, "Password must be at most 16 characters")
+    .required("Password is required"),
+
+  confirmPassword: YUP
+    .string()
+    .oneOf([YUP.ref("password"), null], "Passwords must match")
+    .required("Confirm Password is required")
+});
+
+
+//   .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, "Password must contain at least one letter and one number")
+//   .matches(/^(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/, "Password must contain at least one special character")
+    
+
+
+
+
+
